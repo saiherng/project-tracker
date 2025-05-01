2579f3b14b505d58ee3eb900ddf9f373da70001f
saiherng <saiherng14@gmail.com>
2025-04-30 17:56:18 -0700

    added nav bar styling

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,12 +1,21 @@
-
+'use client';
 
 import { link } from 'fs';
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 
 import { LuBugPlay } from "react-icons/lu";
 
+import classnames from 'classnames';
+
 const NavBar = () => {
+
+    const currentPath = usePathname();
+    //console.log(currentPath);
+
+
+    
 
     const links = [
         {label: "Dashboard", href: "/"},
@@ -15,13 +24,19 @@ const NavBar = () => {
     ]
 
     return (
-    <nav className="flex space-x-6 items-center bg-gray-800 text-white p-5 mb-5 h-20 border-b">
-        <Link href="/"><LuBugPlay /></Link>
+    <nav className="flex space-x-6 items-center bg-slate-700  p-5 mb-5 h-20 border-b">
+        <Link href="/" className='text-stone-200'><LuBugPlay /></Link>
         <ul className='flex space-x-6'>
 
             {links.map(link => 
             <Link key={link.href} 
-                className=' hover:text-indigo-300 transition-colors' href={link.href}>{link.label}</Link>)}
+                className={
+                    classnames({
+                        "text-sky-600": link.href === currentPath,
+                        "text-stone-200": link.href !== currentPath,
+                        "hover:text-sky-500": true,
+                    })
+                }  href={link.href}>{link.label}</Link>)}
 
 
             
