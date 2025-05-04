2c9bc95c3c2a1b63f015d8c95d9d237fdf1bb6e5
saiherng <saiherng14@gmail.com>
2025-05-03 21:58:54 -0700

    added feature to update issue

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -40,7 +40,12 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
     const onSubmit = handleSubmit(async (data) => {
       try {
         setSubmitting(true);
-        await axios.post('/api/issues', data);
+        
+        if (issue)
+          await axios.patch('/api/issues/' + issue.id, data )
+        else
+          await axios.post('/api/issues', data);
+
         router.push('/issues')
         
       } catch (error) {
@@ -67,7 +72,11 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
             render={({field}) => <SimpleMDE placeholder="Description" {...field} /> }/>  
            
             <ErrorMessage>{errors.description?.message}</ErrorMessage>
-            <Button disabled={isSubmitting}> Submit New Issue {isSubmitting && <Spinner/>}</Button>
+            
+            <Button disabled={isSubmitting}> 
+              { issue ? 'Update Issue' : 'Submit New Issue'}
+              {isSubmitting && <Spinner/>}
+              </Button>
 
           </form>
         </div>
