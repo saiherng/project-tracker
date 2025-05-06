d817817a23a6c74fbea61096c654d930b663cc27
saiherng <saiherng14@gmail.com>
2025-05-05 23:29:54 -0700

    changed navbar layout

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -8,6 +8,7 @@ import { LuBugPlay } from "react-icons/lu";
 import classnames from 'classnames';
 import { Container, Flex, Box } from '@radix-ui/themes';
 import { useSession } from 'next-auth/react';
+import Image from 'next/image'
 
 
 const NavBar = () => {
@@ -23,29 +24,32 @@ const NavBar = () => {
 
     ]
     return (
-            <nav className="flex space-x-6 items-center  bg-slate-700 mb-5 h-20 border-b">
+            <nav className=" bg-slate-700 mb-5 h-18 py-5 border-b">
                 <Container>
-                <Flex gap='5' justify='start'>
-                    <Link href="/" className='text-stone-200 flex'><LuBugPlay /></Link>
-                    <div>
-                    <ul className='flex space-x-6'>
-                        {links.map(link => 
-                        <li key={link.href}>
-                            <Link  
-                            className={
-                                classnames({
-                                    "text-gray-400": link.href === currentPath,
-                                    "text-stone-200": link.href !== currentPath,
-                                    "hover:text-yellow-500": true,
-                                })
-                            }  href={link.href}>{link.label}</Link>
-                        </li>
-                        )}
-                    </ul>
-                    </div>
+                <Flex gap='5' justify='between'>
+                    <Flex align='center' gap='5'>
+                        <Link href="/" className='text-stone-200 flex'><LuBugPlay /></Link>
+                        <div>
+                        <ul className='flex space-x-6'>
+                            {links.map(link => 
+                            <li key={link.href}>
+                                <Link  
+                                className={
+                                    classnames({
+                                        "text-gray-400": link.href === currentPath,
+                                        "text-stone-200": link.href !== currentPath,
+                                        "hover:text-yellow-500": true,
+                                    })
+                                }  href={link.href}>{link.label}</Link>
+                            </li>
+                            )}
+                        </ul>
+                        </div>
+                    </Flex>
+    
                     <Box>
                         {status === 'authenticated' && <Link href='/api/auth/signout' className='text-green-700'>Log Out</Link> } 
-                        {status === 'unauthenticated' && <Link href='/api/auth/signin' className='text-red-700'>Log In</Link> } 
+                        {status === 'unauthenticated' && <Link href='/api/auth/signin' className='text-green-700'>Log In</Link> } 
                     </Box>
                 </Flex>
 
@@ -55,6 +59,9 @@ const NavBar = () => {
                 
 
             </nav>
+
+            
+           
         
     
   )
