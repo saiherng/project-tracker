38798648a07c72493179cd9d69ecf94d3dad9a15
saiherng <saiherng14@gmail.com>
2025-05-08 18:43:29 -0700

    added loading skeleton for login link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -8,6 +8,7 @@ import { LuBugPlay } from "react-icons/lu";
 import { Avatar, Box, Container, DropdownMenu, Flex, Text } from '@radix-ui/themes';
 import classnames from 'classnames';
 import { useSession } from 'next-auth/react';
+import { Skeleton } from '@/app/components';
 
 
 
@@ -55,7 +56,7 @@ const NavLinks = () => {
 
 const AuthStatus = () => {
      const {status, data: session} = useSession();
-    if (status === 'loading') return null;
+    if (status === 'loading') return <Skeleton width='3rem'/>;
 
     if (status === 'unauthenticated') 
         return <Link className='nav-link' href='/api/auth/signin'>Login</Link>
